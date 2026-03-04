5fbea54a4badfb83af6ebab2804b15a2918ca8e6
Marco <muxammad.001.com@gmail.com>
2026-03-05 01:08:27 +0500

    Delete fuelmap directory

--- fuelmap/stations/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import index, stations_api
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('api/stations/', stations_api, name='stations_api'),
-]
